03e065cd5fbd0cbd611734c60978c077fd36e18e
Herman Adu <herman.adu@hotmail.com>
2022-02-26 18:13:16 +0000

    form test

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -98,7 +98,34 @@ const Contact = () => {
               <div className="uk-grid-item-match uk-flex-middle uk-width-expand@m">
                 <div className="uk-panel">
                   <div className="uk-margin">
-                    <form name="contact-form" method="post" data-netlify="true">
+                    <form
+                      name="contact-form"
+                      method="post"
+                      data-netlify="true"
+                      data-netlify-honeypot="bot-field"
+                    >
+                      <input type="hidden" name="form-name" value="contact" />
+                      <p>
+                        <label htmlFor="name">Name</label> <br />
+                        <input type="text" id="name" name="name" required />
+                      </p>
+                      <p>
+                        <label htmlFor="email">Email</label> <br />
+                        <input type="email" id="email" name="email" required />
+                      </p>
+                      <p>
+                        <label htmlFor="message">Message</label> <br />
+                        <textarea
+                          id="message"
+                          name="message"
+                          required
+                        ></textarea>
+                      </p>
+                      <p>
+                        <input type="submit" value="Submit message" />
+                      </p>
+                    </form>
+                    {/* <form name="contact-form" method="post" data-netlify="true">
                       <div className="uk-margin">
                         <label className="uk-form-label" htmlFor="name">
                           Name
@@ -150,7 +177,7 @@ const Contact = () => {
                           Send
                         </button>
                       </div>
-                    </form>
+                    </form> */}
                   </div>
                 </div>
               </div>
