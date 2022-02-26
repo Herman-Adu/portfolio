128658d78403c35b89d66ba87514d74bb047822e
Herman Adu <herman.adu@hotmail.com>
2022-02-26 19:47:01 +0000

    form test

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -104,27 +104,14 @@ const Contact = () => {
                       data-netlify="true"
                       data-netlify-honeypot="bot-field"
                     >
-                      <input type="hidden" name="form-name" value="contact" />
-                      <p>
-                        <label htmlFor="name">Name</label> <br />
-                        <input type="text" id="name" name="name" required />
-                      </p>
-                      <p>
-                        <label htmlFor="email">Email</label> <br />
-                        <input type="email" id="email" name="email" required />
-                      </p>
-                      <p>
-                        <label htmlFor="message">Message</label> <br />
-                        <textarea
-                          id="message"
-                          name="message"
-                          required
-                        ></textarea>
-                      </p>
-                      <div data-netlify-captcha></div>
-                      <p>
-                        <input type="submit" value="Submit message" />
-                      </p>
+                      <input name="name" placeholder="Your Name" type="text" />
+                      <input
+                        name="email"
+                        placeholder="name@name.com"
+                        type="email"
+                      />
+                      <textarea name="message" />
+                      <button>Send</button>
                     </form>
                     {/* <form name="contact-form" method="post" data-netlify="true">
                       <div className="uk-margin">
